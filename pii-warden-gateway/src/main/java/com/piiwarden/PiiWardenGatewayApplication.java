4f0f054b935332bc0d1a3faf0545eb54db7669d9
Albin Sunny <psalbin2005@gmail.com>
2026-04-10 23:22:01 +0530

    Initial Setup added

---
+++ pii-warden-gateway/src/main/java/com/piiwarden/PiiWardenGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.piiwarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiiWardenGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiiWardenGatewayApplication.class, args);
+	}
+
+}
